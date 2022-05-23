6339873dc58566ad343e542518d0643203807b43
analiarojas2900 <analia.rojas@duocuc.cl>
2022-05-23 19:27:04 -0400

    commits

--- Restaurante-Web/polls/urls.py
+++ Restaurante-Web/polls/urls.py
@@ -2,8 +2,19 @@ from unicodedata import name
 from django.urls import path
 from .views import index
 from .views import Contacto
+from .views import InicioSeccion
+from .views import MenuNormal
+from .views import MenuVegetariano
+from .views import Registrar
+from .views import Reserva
+
 
 urlpatterns = [
     path('index', index, name='index'),
     path('Contacto', Contacto, name='Contacto'),
+    path('InicioSeccion', InicioSeccion, name='InicioSeccion'),
+    path('MenuNormal', MenuNormal, name='MenuNormal'),
+    path('MenuVegetariano', MenuVegetariano, name='MenuVegetariano'),
+    path('Registrar', Registrar, name='Registrar'),
+    path('Reserva', Reserva, name='Reserva'),
 ]
