034293f2f7b89baf896601f2341ea9459d8e48a1
analiarojas2900 <analia.rojas@duocuc.cl>
2022-06-15 15:10:55 -0400

    commits

--- Restaurante/rest_comida/views.py
+++ Restaurante/rest_comida/views.py
@@ -12,22 +12,6 @@ from rest_framework.permissions import IsAuthenticated
 @csrf_exempt
 @api_view(['GET','POST'])
 @permission_classes((IsAuthenticated,))
-def lista_comida(request):
-    if request.method == 'GET':
-        listaComida =  Comida.objects.all()
-        serializer = ComidaSerializer(listaComida, many = True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        dataP = JSONParser().parse(request)
-        serializer = ComidaSerializer(data=dataP)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET','PUT','DELETE'])
-@permission_classes((IsAuthenticated,))
 def detalle_comida(request, id):
     try:
         comida = Comida.objects.get(idPlato=id)
@@ -35,16 +19,16 @@ def detalle_comida(request, id):
         return Response(status = status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
-        seriaz =  ComidaSerializer(comida)
-        return Response(seriaz.data)
+        seriaLIZER =  ComidaSerializer(comida)
+        return Response(serializer.data)
     elif request.method == "PUT":
         dataP = JSONParser().parse(request)
-        seriaz =  ComidaSerializer(comida, data=dataP)
-        if seriaz.is_valid():
-            seriaz.save()
-            return Response(seriaz.data)
+        serializer =  ComidaSerializer(comida, data=dataP)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         else:
-            return Response(seriaz.errors, status = status.HTTP_400_BAD_REQUEST)            
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)            
     elif request.method == "DELETE":
-        Comida.delete()
+        comida.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
